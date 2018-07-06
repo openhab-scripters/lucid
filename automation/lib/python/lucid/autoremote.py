b698095bd5995bfedf0794dd363098a214630459
B Synnerlig <besynnerlig@users.noreply.github.com>
2018-07-06 18:30:44 +0200

    Added config file for autoremote

--- automation/lib/python/lucid/autoremote.py
+++ automation/lib/python/lucid/autoremote.py
@@ -1,17 +1,15 @@
 import os
+import lucid.config as lucidconfig
 
 def autoremote(message, ttl=300, sender='openHAB'):
     '''
     Sends an autoremote message
     '''
 
-    ARPassw = 'secret'
-    ARKey = 'LONG-KEY-GOES-HERE'
-
     # Use GCM Server for delivery
     cmd = 'curl -s -G "https://autoremotejoaomgcd.appspot.com/sendmessage" ' \
-        + '--data-urlencode "key='+ARKey+'" ' \
-        + '--data-urlencode "password='+ARPassw+'" ' \
+        + '--data-urlencode "key='+lucidconfig.autoremote['key']+'" ' \
+        + '--data-urlencode "password='+lucidconfig.autoremote['password']+'" ' \
         + '--data-urlencode "message='+message+'" ' \
         + '--data-urlencode "sender='+sender+'" ' \
         + '--data-urlencode "ttl='+str(ttl)+'" ' \
