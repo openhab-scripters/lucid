4b53b7438eef8b95890ae3b986f06af34a94020c
B Synnerlig <besynnerlig@users.noreply.github.com>
2018-07-09 11:55:42 +0200

    Adding first example

---
+++ Script Examples/greetings.py
@@ -0,0 +1,41 @@
+# This scripts speaks a random greeting every minute on your Sonos speaker system.
+# 
+# It assumes that you've set up an openHAB a contact items to represent the presence of
+# persons to be greeted. Each item should belong to the item group "G_Presence_Family"
+
+from lucid.rules import rule, addRule
+from lucid.triggers import CronTrigger
+from lucid.speak import tts
+from lucid.utils import greeting, PRIO
+import random
+
+@rule
+class SayHello(object):
+    def getEventTriggers(self):
+        return [
+            CronTrigger('0 0/1 * 1/1 * ? *'), # Runs every minute
+        ]
+
+    def execute(self, modules, inputs):
+        greetings = [greeting(), 'Hello', 'How are you', 'How are you doing', 'Good to see you', 'Long time no see or It\’s been a while']
+        peopleAtHome = []
+        for member in itemRegistry.getItem('G_Presence_Family').getAllMembers():
+            if member.state.toString() == 'OPEN': peopleAtHome.append(member.label)
+        random.shuffle(peopleAtHome)
+        msg = random.choice(greetings)
+        for i in range(len(peopleAtHome)):
+            person = peopleAtHome[i]
+            msg += ' '+person
+            if i+2 == len(peopleAtHome):
+                msg +=' and'
+            elif i+1 == len(peopleAtHome):
+                msg +='.'
+            elif i+2 < len(peopleAtHome):
+                msg +=','
+        tts(msg, PRIO['HIGH'], ttsRoom='Kitchen', ttsVol=42, ttsLang='en-GB', ttsVoice='Brian')
+        tts(msg, PRIO['HIGH'], ttsRoom='Kitchen', ttsVol=42, ttsLang='en-IN', ttsVoice='Aditi')
+        tts(msg, PRIO['HIGH'], ttsRoom='Kitchen', ttsVol=42, ttsLang='en-US', ttsVoice='Matthew')
+        tts(msg, None, ttsRoom='All', ttsLang='de-DE', ttsVoice='Vicki')
+        tts(msg) # Also works if you accept the defaults
+
+addRule(SayHello())
