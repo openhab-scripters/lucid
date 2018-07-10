8952edc4c5b6a137393fb1eecc7d4e40de71ca63
B Synnerlig <besynnerlig@users.noreply.github.com>
2018-07-10 18:59:25 +0200

    Fixed a phrase

--- Script Examples/speakTextInStringItem.py
+++ Script Examples/speakTextInStringItem.py
@@ -6,7 +6,7 @@ from lucid.speak import tts
 @rule
 class SpeakThisPhrase(object):
     """
-    Make openHAB speak a message posting to the openHAB REST API from an external host
+    Make openHAB speak a message that you've posted to the openHAB REST API from an external host
     This script watches an openHAB string item, e.g: String Speak_This "Speak this [%s]" <text> 
     Use curl from an external host to set the item to the text string that shall be spoken
     e.g. curl -X PUT --header "Content-Type: text/plain" --header "Accept: application/json" -d "Hello world" "http://OPENHABHOST:8080/rest/items/Speak_This/state"
